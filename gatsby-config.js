0bf667aff6a406a0eb085e55ca70c027cd1e2973
chrisamurao <freshpotatoes@gmail.com>
2018-12-16 16:24:12 -0800

    changed name, test CI/CD

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
+    title: 'Christopher Amurao',
+    author: 'Christopher Amurao',
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsby-starter-blog-demo.netlify.com/',
     social: {
-      twitter: 'kylemathews'
-    }
+      twitter: 'chrisamurao',
+    },
   },
   plugins: [
     {
